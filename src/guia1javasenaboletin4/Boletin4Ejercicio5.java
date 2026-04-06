29339353a453b8a8237b07588d2ea5b5b0d779f4
johnalexf <johnalexfr@gmail.com>
2026-04-06 11:08:51 -0500

    feat: solucion ejercicio 5

---
+++ src/guia1javasenaboletin4/Boletin4Ejercicio5.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guia1javasenaboletin4;
+
+/**
+ *
+ * @author JOHN FORERO
+ */
+public class Boletin4Ejercicio5 {
+    
+    public static void solucion(){
+    
+        System.out.println("5. Leer por teclado dos tablas de 10 números enteros y mezclarlas en una tercera de la forma: el 1º de A, el 1º \n" +
+                            "de B, el 2º de A, el 2º de B, etc.");
+        
+        int cantNumeros = 10;
+        int[] capturarNumeros = new int[cantNumeros];
+        int[] numeros1 = new int[cantNumeros];
+        int[] numeros2 = new int[cantNumeros];
+        int[] numerosMezcla = new int[cantNumeros*2];
+        
+        for(int i=0; i < 2 ; i++ ){
+            System.out.printf("%nIngresar los datos de la tabla #%d %n", i+1);
+            
+            for( int j=0 ; j < capturarNumeros.length ; j++){
+                System.out.printf("Digita el numero %d = ", j+1);
+                capturarNumeros[j]= Entrada.entero();
+            }
+            if(i == 0) numeros1 = capturarNumeros.clone();
+            else numeros2 = capturarNumeros.clone();
+            
+        }
+        
+        for(int i=0 ; i < numeros1.length ; i++ ){
+            numerosMezcla[2*i] = numeros1[i];
+            numerosMezcla[2*i+1] = numeros2[i];
+        }
+        
+        
+        System.out.println("La tabla con los datos mezclados y en el orden solicitados es:");
+        
+        for(int i=0; i< numerosMezcla.length ; i++){
+            System.out.printf("%d %n", numerosMezcla[i]);
+        }
+           
+        
+        
+        
+        
+    }
+}
--- src/guia1javasenaboletin4/Guia1JavaSENABoletin4.java
+++ src/guia1javasenaboletin4/Guia1JavaSENABoletin4.java
@@ -20,7 +20,8 @@ public class Guia1JavaSENABoletin4 {
         //Boletin4Ejercicio1.solucion();
         //Boletin4Ejercicio2.solucion();
         //Boletin4Ejercicio3.solucion();
-        Boletin4Ejercicio4.solucion();
+        //Boletin4Ejercicio4.solucion();
+        Boletin4Ejercicio5.solucion();
         
     }
     
